7b2a8b3790ba660e672e35c4fdd3d963191afe81
parsaMARXx <parsamarx@gmail.com>
2025-01-07 07:04:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.once('ready', () => {
+  console.log(`Ready! Logged in as ${client.user.tag}`);
+
+  // Define your command
+  const command = new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!');
+
+  // Register the command
+  client.application.commands.create(command);
 });
 
-client.on(Events.MessageCreate, (message) => {
-  if (message.author.bot) return;
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
 
-  if (message.content === '!ping') {
-    message.reply('Pong!');
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
   }
 });
 
-client.login('YOUR_BOT_TOKEN');
+  client.login('YOUR_BOT_TOKEN');
